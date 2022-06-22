8677e212181f2baea14b703a14842b5c276172d5
Ruslan Yun <leonemsolis@gmail.com>
2022-06-22 09:24:29 +0600

    first commit

---
+++ chunk.h
@@ -0,0 +1,14 @@
+//
+// Created by znnxzz on 22-Jun-22.
+//
+
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+#endif //CLOX_CHUNK_H
