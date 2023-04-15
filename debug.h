2af8750bfc541b73ebbe477dd24f8cf7af8f1cd7
Ruslan Yun <leonemsolis@gmail.com>
2023-04-15 21:46:03 +0600

    Add constant operator & value

--- debug.h
+++ debug.h
@@ -4,6 +4,8 @@
 #include "chunk.h"
 
 void disassembleChunk(Chunk* chunk, const char* name);
+
+// Returns the offset of the beggining of the next instruction
 int disassembleInstruction(Chunk* chunk, int offset);
 
 #endif
